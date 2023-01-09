fa44c7dae4066cc66eafc534db14dd6102233e3f
lcharvy <lison.charvy@insa-lyon.fr>
2023-01-09 15:35:02 +0100

    add surface method and remove step2

--- sphere/sphere.py
+++ sphere/sphere.py
@@ -10,13 +10,13 @@ class Sphere(object):
 
     # *** STEP 2 ***
     # uncomment the following 2 lines
-    def __str__(self):
-        return '%s(%s)' % (self.__class__.__name__, self.radius)
+    #def __str__(self):
+    #    return '%s(%s)' % (self.__class__.__name__, self.radius)
 
     def surface(self):
         # *** STEP 3 ***
-        # uncomment the following line
-        #return 4.0 * 3.1416 * self.radius ** 2
+         #uncomment the following line
+        return 4.0 * 3.1416 * self.radius ** 2
         pass
 
     def volume(self):
